70017c0c5520fcc7d159d38ddde9112f4ea3cb0d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-09 14:48:36 +0430

    Fix version and reformat code

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup (
-	name = 'pyis',
-	packages = find_packages(),
-	version = '0.15.2',
-	author = 'Amirhossein Mohammadi',
-	license = 'MIT',
-	author_email = "amirhosseinmohammadi1380@yahoo.com",
-    description = "A library to check your values in your projects",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/BlackIQ/pyis"
+setup(
+    name='pyis',
+    packages=find_packages(),
+    version='1.9.3',
+    author='Amirhossein Mohammadi',
+    license='MIT',
+    author_email="amirhosseinmohammadi1380@yahoo.com",
+    description="A library to check your values in your projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlackIQ/pyis"
 )
